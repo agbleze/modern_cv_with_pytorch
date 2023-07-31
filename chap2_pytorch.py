eb66815555f023ca2949b3e4001e359a743d6747
agbleze <lagbleze@st.ug.edu.gh>
2023-07-31 20:01:34 +0200

    Update chap2_pytorch.py

--- chap2_pytorch.py
+++ chap2_pytorch.py
@@ -12,4 +12,57 @@ out = x.pow(2).sum()
 out.backward()
 x.grad
 
-# %%
+# %% building NN with pytorch on toy dataset
+x = [[1,2], [3, 4], [5, 6], [7, 8]]
+y = [[3], [7], [11], [15]]
+
+x = torch.tensor(x).float()
+Y = torch.tensor(y).float()
+
+#%%
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+x.to(device)
+Y = Y.to(device)
+
+#%%
+
+import torch.nn as nn
+
+#%%
+
+class MyNeuralNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.input_to_hidden_layer = nn.Linear(2, 8)
+        self.hidden_layer_activation = nn.ReLU()
+        self.hidden_to_output_layer = nn.Linear(8, 1)
+        
+    def forward(self, x):
+        x = self.input_to_hidden_layer(x)
+        x = self.hidden_layer_activation(x)
+        x = self.hidden_to_output_layer(x)
+        return x
+    
+# feedforward propagation always has to be defined as forwar func
+#%%
+torch.manual_seed(2023)
+mynet = MyNeuralNet().to(device)
+
+#%% checking weight and bias of layer
+mynet.input_to_hidden_layer.weight
+
+#%% get all parameters of the nn
+mynet.parameters()
+
+# obtain parameters by looping through
+
+for par in mynet.parameters():
+    print(par)
+
+            
+
+
+
+
