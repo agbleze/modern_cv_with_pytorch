9d44c7048175a108b557c4e6eec53fbdb2564df4
agbleze <lagbleze@st.ug.edu.gh>
2023-08-02 18:36:18 +0200

    Create NN_fashionMnist.py

---
+++ chap3_DNN_pytorch/NN_fashionMnist.py
@@ -0,0 +1,20 @@
+#%%
+from torch.utils.data import Dataset, DataLoader
+import torch
+import torch.nn as nn
+import numpy as np
+import matplotlib.pyplot as plt
+from torchvision import datasets
+
+
+#%%
+
+
+
+
+
+
+
+
+
+
