2a4b3849469a9f199a70c244c42275d89f074987
agbleze <lagbleze@st.ug.edu.gh>
2023-08-04 09:42:16 +0200

    Update NN_fashionMnist.py

--- chap3_DNN_pytorch/NN_fashionMnist.py
+++ chap3_DNN_pytorch/NN_fashionMnist.py
@@ -245,12 +245,72 @@ def get_data():
     trn_dl = DataLoader(train, batch_size=32, shuffle=True)
     
     val_data = FMINISTDataset(val_images, val_targets)
-    val_dl = DataLoader(val_data, batch_size=len(val_data))
+    val_dl = DataLoader(val_data, batch_size=len(val_images))
     return trn_dl, val_dl
 
 
 
+#%% define val loss
+def val_loss(x, y, model, loss_fn):
+    model.eval()
+    with torch.no_grad():
+        prediction = model(x)
+        val_loss = loss_fn(prediction, y)
+        return val_loss.item()
+    
+    
+#%% fetch training and validation data
+trn_dl, val_dl = get_data()
+model, loss_fn, optimizer = get_model()
 
+#%% train model
+train_losses, train_accuracies = [], []
+val_losses, val_accuracies = [], []
+
+for epoch in range(5):
+    print(epoch)
+    train_epoch_losses, train_epoch_accuracies = [], []
+    
+    for ix, batch in enumerate(iter(trn_dl)):
+        x, y = batch
+        batch_loss = train_batch(x, y, model, loss_fn, optimizer)
+        train_epoch_losses.append(batch_loss)
+    train_epoch_loss = np.array(train_epoch_losses).mean()
+    
+    for ix, batch in enumerate(iter(trn_dl)):
+        x, y = batch
+        is_correct = accuracy(x, y, model)
+        train_epoch_accuracies.extend(is_correct)
+    train_epoch_accuracy = np.mean(train_epoch_accuracies)
+    
+    ##  cal val loss and accuracy
+    for ix, batch in enumerate(iter(val_dl)):
+        x, y = batch
+        val_is_correct = accuracy(x, y, model)
+        validation_loss = val_loss(x,y,model)
+        val_epoch_accuracy = np.mean(val_is_correct)      
+        
+    train_losses.append(train_epoch_loss)
+    train_accuracies.append(train_epoch_accuracy)
+    val_losses.append(validation_loss)
+    val_accuracies.append(val_epoch_accuracy)
+    
+
+#%% visualize results
+epochs = np.arange(5)+1
+import matplotlib.ticker as mticker
+
+#%%
+plt.subplot(211)
+plt.plot(epochs, train_losses, 'bo', label='Traing loss')  
+plt.plot(epochs, val_losses, 'r', label='validation loss')
+plt.gca().xaxis.set_major_locator(mticker.MultipleLocator(1))
+plt.title('Training and validation loss when batch size is 32')      
+plt.xlabel('Epochs')
+plt.ylabel('loss')
+plt.legend()
+plt.grid('off')
+plt.show()
 
 
 
