c7440f78584492defdb8ab16a0b40a49059b3523
agbleze <lagbleze@st.ug.edu.gh>
2023-08-08 21:11:35 +0200

    Update cnn_working_details.py

--- chap4_CNN/cnn_working_details.py
+++ chap4_CNN/cnn_working_details.py
@@ -1,2 +1,42 @@
 
 #%%
+import torch
+from torch import nn
+from torch.utils.data import TensorDataset, Dataset, DataLoader
+from torch.optim import SGD, Adam
+from torchvision import datasets
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#%%  ##### 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+#%% create a toy dataset
+X_train = torch.tensor([[[[1,2,3,4], 
+                          [2,3,4,5],
+                          [5,6,7,8],
+                          [1,3,4,5]
+                          ]
+                         ],
+                        [[[-1,2,3,-4],
+                          [2,-3,4,5],
+                          [-5,6,-7,8],
+                          [-1,-3,-4,-5]
+                          ]
+                         ]
+                        ]
+                       ).to(device)
+
+X_train /= 8   # normalize with max value in data to get values 0 - 1
+y_train = torch.tensor([0,1]).to(device).float()
+
+
+
+
+
+
+
+
+
+
