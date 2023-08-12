2777538fae6134bc659cf33cd6eb0ae1de0b44e8
agbleze <lagbleze@st.ug.edu.gh>
2023-08-12 23:40:55 +0200

    Update gender_age_pred.py

--- chap5_transfer_learning/P1_Facial_Keypoints/gender_age_pred.py
+++ chap5_transfer_learning/P1_Facial_Keypoints/gender_age_pred.py
@@ -113,6 +113,108 @@ def get_model():
             x = self.intermediate(x)
             age = self.age_classifier(x)
             gender = self.gender_classifier(x)
+            return gender, age
+    
+    model.classifier = ageGenderClassifier()
+    
+    gender_criterion = nn.BCELoss()
+    age_criterion = nn.L1Loss()
+    loss_functions = gender_criterion, age_criterion
+    
+    optimizer = optim.Adam(model.parameters(), lr=1e-4)
+    return model.to(device), loss_functions, optimizer
+
+
+#%% 
+model, criterion, optimizer = get_model()
+
+#%% ### 
+def train_batch(data, model, optimizer, criterion):
+    model.train()
+    ims, age, gender = data
+    optimizer.zero_grad()
+    pred_gender, pred_age = model(ims)
+    gender_criterion, age_criterion = criterion
+    gender_loss = gender_criterion(pred_gender.squeeze(), gender)
+    age_loss = age_criterion(pred_age.squeeze(), age)
+    total_loss = gender_loss + age_loss
+    total_loss.backward()
+    optimizer.step()
+    return total_loss
+
+#%%
+def validate_batch(data, model, criteria):
+    ims, age, gender = data
+    model.eval()
+    with torch.no_grad():
+        pred_gender, pred_age = model(ims)
+        gender_criterion, age_criterion = criteria
+        gender_loss = gender_criterion(pred_gender.squeeze(), gender)
+        age_loss = age_criterion(pred_age.squeeze(), age)
+        total_loss = gender_loss + age_loss
+        pred_gender = (pred_gender > 0.5).squeeze()
+        gender_acc = (pred_gender == gender).float().sum()
+        
+        age_mae = torch.abs(age - pred_age).float().sum()
+        return total_loss, gender_acc, age_mae
+    
+    
+#%% train the model over 5 epochs
+import time
+
+model, criteria, optimizer = get_model()
+val_gender_accuracies = []
+val_age_maes = []
+train_losses = []
+val_losses = []
+n_epochs = 5
+best_test_loss = 1000
+start = time.time()
+
+for epoch in range(n_epochs):
+    epoch_train_loss, epoch_test_loss = 0, 0
+    val_age_mae, val_gender_acc, ctr = 0,0,0
+    _n=len(train_loader)
+    for ix, data in enumerate(train_loader):
+        loss = train_batch(data, model, optimizer, criteria)
+        epoch_train_loss += loss.item()
+        
+    for ix, data in enumerate(test_loader):
+        loss, gender_acc, age_mae = validate_batch(data, model, criteria)
+        epoch_test_loss += loss.item()
+        val_age_mae += age_mae
+        val_gender_acc += gender_acc
+        ctr += len(data[0])
+        
+    # cal overall accuracy of age pred and gender classifications
+    val_age_mae /= ctr
+    val_gender_acc /= ctr
+    epoch_train_loss /= len(train_loader)
+    epoch_test_loss /= len(test_loader)
+        
+    #log metrics of each epoch
+    elapsed = time.time() -start
+    best_test_loss = min(best_test_loss, epoch_test_loss)
+    print('{}/{} ({:.2f}s - {:.2f}s remaining)'.format(epoch+1, n_epochs, time.time()-start,
+                                                       (n_epochs-epoch)*(elapsed/epoch+1)
+                                                       )
+          )
+    info = f'''Epoch: {epoch+1:03d}\tTrain Loss: {epoch_train_loss:.3f}
+                \tTest: {epoch_test_loss:.3f}
+                \tBest test Loss: {best_test_loss:.4f}
+            '''
+    info += f'\nGender Accuracy: {val_gender_acc*100:.2f}%\tAge MAE: \
+                {val_age_mae:.2f}\n'
+    print(info)
+    
+    val_gender_accuracies.append(val_gender_acc)
+    val_age_maes.append(val_age_mae)
+        
+        
+
+    
+    
+    ## system application and product in data processing
 
 
 
