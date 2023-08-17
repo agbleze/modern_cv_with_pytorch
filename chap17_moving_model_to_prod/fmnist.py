77f82be245d35a60ff9ae674d5c448fcdc8a7572
agbleze <lagbleze@st.ug.edu.gh>
2023-08-17 22:00:37 +0200

    Update fmnist.py

--- chap17_moving_model_to_prod/fmnist.py
+++ chap17_moving_model_to_prod/fmnist.py
@@ -2,3 +2,10 @@
 from torch_snippets import *
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+class FMNIST(nn.Module):
+    classes = ['T-shit/top', 'Trouser', 'Pullover', 'Dress',\
+             'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot'
+             ]
+    
+    
