1c7ffd8359ae6b49613d2305308c0de82642d245
agbleze <lagbleze@st.ug.edu.gh>
2023-08-21 19:52:26 +0200

    Update SingleShotDetector_300.py

--- chap8_advanced_obj_detection/SingleShotDetector_300.py
+++ chap8_advanced_obj_detection/SingleShotDetector_300.py
@@ -27,6 +27,62 @@ num_classes = len(label2target)
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 #%%
+import collections, os, torch
+from PIL import Image
+from torchvision import transforms
+import glob
+
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225]
+                                 )
+
+denormalize = transforms.Normalize(
+                            mean=[-0.485/0.229, -0.456/0.224, -0.406/0.255],
+                            std=[0.229, 0.224, 0.225]
+                        )
+
+def preprocess_image(img):
+    img = torch.tensor(img).permute(2,0,1)
+    img = normalize(img)
+    return img.to(device).float()
+
+class OpenDataset(Dataset):
+    w, h = 300, 300
+    def __init__(self, df, image_dir=IMAGE_ROOT):
+        self.image_dir = image_dir
+        self.files = glob.glob(self.image_dir+'/*')
+        self.df = df
+        self.image_infos = df.ImageID.unique()
+        logger.info(f'{len(self)} items loaded')
+        
+    def __getitem__(self, ix):
+        # load images and masks
+        image_id = self.image_infos[ix]
+        img_path = find(image_id, self.files)
+        img = Image.open(img_path).convert("RGB")
+        img = np.array(img.resize((self.w, self.h), resample=Image.BILINEAR))/255.
+        data = df[df['ImageID']==image_id]
+        labels = data['LabelName'].values.tolist()
+        data = data[['XMin','YMin', 'XMax','YMax']].values
+        data[:,[0,2]] *= self.w
+        data[:,[1,3]] *= self.h
+        boxes = data.astype(np.uint32).tolist() 
+        return img, boxes, labels
+    
+    def collate_fn(self, batch):
+        images, boxes, labels = [], [], []
+        for item in batch:
+            img, image_boxes, image_labels = item
+            img = preprocess_image(img)[None]
+            images.append(img)
+            boxes.append(torch.tensor(image_boxes).float().to(device)/300.)
+            labels.append(torch.tensor([label2target[c] for c in image_labels]).long().to(device))
+        images = torch.cat(images).to(device)
+        return images, boxes, labels
+    
+    def __len__(self):
+        return len(self.image_infos)
+            
 
 
 
