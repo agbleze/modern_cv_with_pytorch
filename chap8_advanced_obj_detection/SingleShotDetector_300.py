3dd0f49f15654a9f1aaf1a0465021baea96ad811
agbleze <lagbleze@st.ug.edu.gh>
2023-08-21 19:07:35 +0200

    Update SingleShotDetector_300.py

--- chap8_advanced_obj_detection/SingleShotDetector_300.py
+++ chap8_advanced_obj_detection/SingleShotDetector_300.py
@@ -3,11 +3,30 @@
 import os
 
 if not os.path.exists('open-images-bus-trucks'):
-    !wget --quite https
+    !wget --quite https://www.dropbox.com/s/agmzwk95v96ihic/open-images-bus-trucks.tar.xz
+    !tar -xf open-images-bus-trucks.tar.xz
+    !rm open-images-bus-trucks.tar.xz
+    !git clone https://github.com/sizhky/ssd-utils/
+!cd ssd-utils
 
 
+#%%
+from torch_snippets import *
+DATA_ROOT = '../open-images-bus-trucks/'
+IMAGE_ROOT = f'../{DATA_ROOT}/images'
+DF_RAW = df = pd.read_csv(f'{DATA_ROOT}/df.csv')
 
+df = df[df['ImageID'].isin(df['ImageID'].unique().tolist())]
 
+label2target = {l:t+1 for t,l in enumerate(DF_RAW['LabelName'].unique())}
+label2target['background'] = 0
+target2label = {t:l for l,t in label2target.items()}
+background_class =  label2target['background']
+num_classes = len(label2target)
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+#%%
 
 
 
