8fe8135973061a939f1ea08741334dd0ab7f3a43
agbleze <lagbleze@st.ug.edu.gh>
2023-08-21 21:16:44 +0200

    Update SingleShotDetector_300.py

--- chap8_advanced_obj_detection/SingleShotDetector_300.py
+++ chap8_advanced_obj_detection/SingleShotDetector_300.py
@@ -84,5 +84,86 @@ class OpenDataset(Dataset):
         return len(self.image_infos)
             
 
+#%%
+from sklearn.model_selection import train_test_split
+
+trn_ids, val_ids = train_test_split(df.ImageID.unique(), test_size=0.1, random_state=99)
+trn_df, val_df = df[df['ImageID'].isin(trn_ids)], df[df['ImageID'].isin(val_ids)]
+len(trn_df), len(val_df)
+
+train_ds = OpenDataset(trn_df)
+test_ds = OpenDataset(val_df)
+
+train_loader = DataLoader(train_ds, batch_size=4, collate_fn=train_ds.collate_fn, drop_last=True)
+test_loader = DataLoader(test_ds, batch_size=4, collate_fn=test_ds.collate_fn, drop_last=True)
 
 
+#%%
+
+def train_batch(inputs, model, criterion, optimizer):
+    model.train()
+    N = len(train_loader)
+    images, boxes, labels = inputs
+    _regr, _clss, = model(images)
+    loss = criterion(_regr, _clss, boxes, labels)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    return loss
+
+@torch.no_grad()
+def validate_batch(inputs, model, criterion):
+    model.eval()
+    images, boxes, labels = inputs
+    _regr, _clss = model(images)
+    loss = criterion(_regr, _clss, boxes, labels)
+    return loss
+
+
+#%%
+from model import SSD300, MultiBoxLoss
+from detect import *
+
+#%%
+n_epochs = 3
+model = SSD300(num_classes, device)
+optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-5)
+criterion = MultiBoxLoss(priors_cxcy=model.priors_cxcy, device=device)
+
+log = Report(n_epochs=n_epochs)
+logs_to_print = 5
+
+
+#%%
+for epoch in range(n_epochs):
+    _n = len(train_loader)
+    for ix, inputs in enumerate(train_loader):
+        loss = train_batch(inputs, model, criterion, optimizer)
+        pos = (epoch + (ix+1)/_n)
+        log.record(pos, trn_loss=loss.item(), end='\r')
+        
+    _n = len(test_loader)
+    for ix, inputs in enumerate(test_loader):
+        loss = validate_batch(inputs, model, criterion)
+        pos = (epoch + (ix+1)/_n)
+        log.record(pos, val_loss=loss.item(), end='\r')
+        
+        
+#%%
+image_paths = Glob(f'{DATA_ROOT}/images/*')
+image_id = choose(test_ds.image_infos)
+img_path = find(image_id, test_ds.files)
+original_image = Image.open(img_path, mode='r')
+original_image = original_image.convert('RGB')
+
+#%%
+image_paths = Glob(f'{DATA_ROOT}/images/*')
+for _ in range(3):
+    image_id = choose(test_ds.image_infos)
+    img_path = find(image_id, test_ds.files)
+    original_image = Image.open(img_path, mode='r')
+    bbs, labels, scores = detect(original_image, model, min_score=0.9, max_overlap=0.5, top_k=200, device=device)
+    labels = [target2label[c.item()] for c in labels]
+    label_with_conf = [f'{l}@{s:.2f}' for l,s in zip(labels, scores)]  
+    print(bbs, label_with_conf) 
+    show(original_image, bbs=bbs, texts=label_with_conf, text_sz=10) 
