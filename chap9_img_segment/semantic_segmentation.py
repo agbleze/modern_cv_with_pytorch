81d18533b4d6a654c0403dcdbe5858bef92e6687
agbleze <lagbleze@st.ug.edu.gh>
2023-08-24 23:47:56 +0200

    Update semantic_segmentation.py

--- chap9_img_segment/semantic_segmentation.py
+++ chap9_img_segment/semantic_segmentation.py
@@ -29,7 +29,73 @@ class SegData(Dataset):
         self.split = split
     def __len__(self):
         return len(self.items)
+    def __getitem__(self,ix):
+        image = read(f'dataset1/images_prepped_{self.split}/{self.items[ix]}.png', 1)
+        image = cv2.resize(image, (224, 224))
+        mask = read(f'dataset1/annotations_prepped_{self.split}/{self.items[ix]}.png')
+        mask = cv2.resize(mask, (224, 224))
+        return image, mask
+    def choose(self): return self[randint(len(self))]
+    
+    def collate_fn(self, batch):
+        ims, masks = list(zip(*batch))
+        ims = torch.cat([tfms(im.copy()/255.)[None] for im in ims]).float().to(device)
+        ce_masks = torch.cat([torch.Tensor(mask[None]) for mask in masks]).long().to(device)
+        return ims, ce_masks
+    
+    
+#%%
+trn_ds = SegData('train')
+val_ds = SegData('test')
+trn_dl = DataLoader(trn_ds, batch_size=4, shuffle=True, collate_fn=trn_ds.collate_fn)
+val_dl = DataLoader(val_ds, batch_size=1, shuffle=True, collate_fn=val_ds.collate_fn)
+
+show(trn_ds[10][10])
+
+#%%
+
+def conv(in_channels, out_channels):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(inplace=True)
+    )
+    
+def up_conv(in_channels, out_channels):
+    return nn.Sequential(
+        nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2),
+        nn.ReLU(inplace=True)
+    )
+    
+#%%
+from torchvision.models import vgg16_bn
+
+class UNet(nn.Module):
+    def __init__(self, pretrained=True, out_channels=12):
+        super().__init__()
         
+        self.encoder = vgg16_bn(pretrained=pretrained).features
+        self.block1 = nn.Sequential(*self.encoder[:6])
+        self.block2 = nn.Sequential(*self.encoder[6:13])
+        self.block3 = nn.Sequential(*self.encoder[13:20])
+        self.block4 = nn.Sequential(*self.encoder[20:27])
+        self.block5 = nn.Sequential(*self.encoder[27:34])
+        
+        self.bottleneck = nn.Sequential(*self.encoder[34:])
+        self.conv_bottleneck = conv(512, 1024)
+        
+        self.up_conv6 = up_conv(1024, 512)
+        self.conv6 = conv(512 + 512, 512)
+        self.up_conv7 = up_conv(512, 256)
+        self.conv7 = conv(256 + 512, 256)
+        self.up_conv8 = up_conv(256, 128)
+        self.conv8 = conv(128 + 256, 128)
+        self.up_conv9 = up_conv(128, 64)
+        self.conv9 = conv(64 + 128, 64)
+        self.up_conv10 = up_conv(64, 32)
+        self.conv10 = conv(32 + 64, 32)
+        self.conv11 = nn.Conv2d(32, out_channels, kernel_size=1)
+
 
 
 
