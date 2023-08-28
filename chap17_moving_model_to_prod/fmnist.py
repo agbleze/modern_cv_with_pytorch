5c5e2561dbe9c029bb03221bb24b90edf2294c76
agbleze <lagbleze@st.ug.edu.gh>
2023-08-28 21:42:54 +0200

    Update fmnist.py

--- chap17_moving_model_to_prod/fmnist.py
+++ chap17_moving_model_to_prod/fmnist.py
@@ -7,11 +7,25 @@ class FMNIST(nn.Module):
     classes = ['T-shit/top', 'Trouser', 'Pullover', 'Dress',\
              'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot'
              ]
-    def __init__(self):
+    def __init__(self, fpath = "fmnist.weight.pth"):
         super().__init__()
         self.model = nn.Sequential(
-            nn.Conv2d(),
-        )
+                            nn.Linear(28*28, 1000),
+                            nn.ReLU(),
+                            nn.Linear(1000, 10)
+                        ).to(device)
+        self.model.load_state_dict(torch.load(fpath))
+        logger.info('Loaded FMNIST Model')
+    
+    @torch.no_grad()    
+    def forward(self, x):
+        x = x.view(1, -1).to(device)
+        pred = self.model(x)
+        pred = F.softmax(pred, -1)[0]
+        conf, clss = pred.max(-1)
+        clss = self.classes[clss.cpu().item()]
+        return conf.item(), clss
+        
     
     
     
